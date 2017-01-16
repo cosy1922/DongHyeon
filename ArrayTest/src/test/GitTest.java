3ece5dc3b896d9387e372afe1e120ce7e8d29f2d
it <it@DESKTOP-KKAQ0RK>
2017-01-16 14:05:51 +0900

    한글 깃테스트

--- ArrayTest/src/test/GitTest.java
+++ ArrayTest/src/test/GitTest.java
@@ -3,7 +3,6 @@ package test;
 public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("git test");
-		System.out.println("±êÅ×½ºÆ®");
+		System.out.println("ê¹ƒí…ŒìŠ¤íŠ¸");
 	}
 }
- 
